f273942b1aea56077d1f6fefddd1002edaef6d70
nanodg <40233834+nanodg@users.noreply.github.com>
2021-12-20 14:11:47 +0700

    change worker name

--- verus.sh
+++ verus.sh
@@ -1,4 +1,4 @@
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xvf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHg6xgrZ2fiL2JHpArodHicy7TaWFRZZcP.FREE2 -p x --cpu 4
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHg6xgrZ2fiL2JHpArodHicy7TaWFRZZcP.VPS -p x --cpu 4
